e5b601be51bbfd32839cd3ae2a1e1de857d6af0b
David Vuong <david.vuong256@gmail.com>
2016-03-07 00:17:13 +1100

    feat(lib): add skiplist.insert method

--- app.js
+++ app.js
@@ -5,13 +5,21 @@
  * Ignore this file. This is a file used just for manual testing. Everything
  * will look like context-less gibberish here!
  *
- * Copyright (c) 2015 David Vuong <david.vuong256@gmail.com>
+ * Copyright (c) 2016 David Vuong <david.vuong256@gmail.com>
  * Licensed MIT
  */
 'use strict';
+import _ from 'lodash';
+
 import BinarySearchTree from './lib/bst';
 import CircularLinkedList from './lib/ll';
 import BinaryHeap from './lib/heap';
 import RedBlackTree from './lib/rbt';
+import SkipList from './lib/skiplist';
 
 import {bst2dot} from './helpers/tree';
+
+const list = new SkipList();
+
+_.shuffle(_.range(50)).map(i => list.insert(i));
+list.toString();
---
+++ tests/lib/skiplist.spec.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+/**
+ * es6-ds/tests/lib/skiplist.spec.js
+ *
+ * Copyright (c) 2016 David Vuong <david.vuong256@gmail.com>
+ * Licensed MIT
+ */
+'use strict';
+import chai from 'chai';
+import _ from 'lodash';
+import SkipList from '../../lib/skiplist';
+
+describe('SkipList', () => {
+    describe('.insert()', () => {
+        let list;
+        beforeEach(() => {
+            list = new SkipList();
+        });
+
+        it('should ...', () => {
+
+        });
+    });
+
+    describe('.get()', () => {
+
+    });
+
+    describe('.remove()', () => {
+
+    });
+});
